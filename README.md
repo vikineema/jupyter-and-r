5f7a9b6fe0bde1d25a1094a15a637d9a0b8d11c0
Victoria Neema <victoria@kartoza.com>
2025-12-28 23:48:25 +0300

    Complete jupyter and R setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jupyter-and-r
 
+Adding the R kernel to jupyterlab is based on [Setting up Jupyter Lab with Additional Kernels in NixOS](https://yannipapandreou.github.io/posts/jupyter-kernels-nix/index.html).
+
+Adding additional R packages:
+https://nixos.wiki/wiki/R
+
--- flake.nix
+++ flake.nix
@@ -9,22 +9,49 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-    libs = with pkgs; [
-      #gdal
-      #expat
-    ];
+    rWrapper = pkgs.rWrapper;
+    # Include desired R packages desired here.
+    RPackages = with pkgs.rPackages; [ 
+        tidyverse
+        IRkernel
+      ]; 
+    rEnv = rWrapper.override {packages = RPackages; };
+    setupRKernel = pkgs.writeShellScript "r-kernel-setup" ./r_kernel.sh;
+    setupUV = pkgs.writeShellScript "uv-setup" ./uv_setup.sh;
+
   in
   {
     devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [ pkgs.uv ] ++ libs;
+      buildInputs = with pkgs; [ 
+        uv
+        rEnv
+        ];
 
       shellHook = ''
         export VIRTUAL_ENV="$(pwd)/.venv"
-        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
+        
+        # Check where the python kernel is installed in the virtual
+        # environment using "jupyter kernelspec list" and set KERNEL_DIR
+        # accordingly.
+        export KERNEL_DIR="$VIRTUAL_ENV/share/jupyter/kernels"
 
+        # Install the R kernel spec into the virtual environment's
+        # Jupyter kernels directory.
+        export IRKERNEL_DIR="$KERNEL_DIR/ir"
+        mkdir -p "$IRKERNEL_DIR"
+
+        # Copy the files using interpolation
+        cp -r ${pkgs.rPackages.IRkernel}/library/IRkernel/kernelspec/* $IRKERNEL_DIR/
+
+        # Add write permission
+        chmod -R u+w $IRKERNEL_DIR
+        echo "Jupyter with R kernel is ready. Run: 'jupyter lab' to launch"
+
+        # Install python packages using uv
+        # Include desired python packages in requirements.txt
         uv add -r requirements.txt
         uv sync
-        source .venv/bin/activate
+        source "$VIRTUAL_ENV"/bin/activate
       '';
     };
   };
